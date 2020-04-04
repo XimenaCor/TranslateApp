57b6d9144a496def6d40b0d0799d1f6c10ae4789
ximena <ximena.cordero@nearshorecode.com>
2020-04-03 21:55:00 -0400

    First commit for this repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
-
+import {TranslateService} from '@ngx-translate/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-translate';
+
+  langs: string[] = [];
+
+  constructor(
+    private translate: TranslateService
+    ) {
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+    this.translate.addLangs(['es', 'en']);
+    this.langs = this.translate.getLangs();
+    this.translate.stream('HELLO')
+    .subscribe((res: string) => {
+        console.log(res);
+        //=> 'hello world'
+      });
+      this.translate.stream('GREETING', {name: 'Cristian'})
+      .subscribe((res: string) => {
+        console.log(res);
+      });
+  }
+
+  changeLang(lang: string) {
+    this.translate.use(lang);
+  }
 }
